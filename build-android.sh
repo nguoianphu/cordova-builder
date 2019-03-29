cdc32187f08224ab9c3f948fdfbb4914126ec693
Nguoi An Phu <mrfly911@gmail.com>
2019-03-29 10:34:26 +0000

    CORRECT phonegap build --release will create app-release-unsigned.apk

--- build-android.sh
+++ build-android.sh
@@ -12,12 +12,12 @@ phonegap plugin list
 echo "Building apk for Android"
 phonegap build android --release
 
-#cd ..
-cd $TRAVIS_BUILD_DIR
+echo "Built the following apk(s):"
+# app-release-unsigned.apk
+ls -la $TRAVIS_BUILD_DIR/platforms/android/app/build/outputs/apk/release
+
+cd ..
+# cd $TRAVIS_BUILD_DIR
 # HOME="/home/travis"
 # TRAVIS_BUILD_DIR="/home/travis/build/nguoianphu/cordova-builder"
 # TRAVIS_REPO_SLUG="nguoianphu/cordova-builder"
-
-echo "Built the following apk(s):"
-# app-release-unsigned.apk
-ls -la platforms/android/app/build/outputs/apk/release
