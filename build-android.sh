a66c3716f54950082569372cf7c6a5106719b8e4
nguoianphu <nguoianphu@gmail.com>
2019-03-29 21:35:41 +0700

    Can not run app after building with RELEASE mode

--- build-android.sh
+++ build-android.sh
@@ -13,11 +13,11 @@ cordova plugin add cordova-plugin-whitelist
 cordova plugin ls
 
 echo "Building apk for Android"
-cordova build android --release
+cordova build android
 
 echo "Built the following apk(s):"
 # app-release-unsigned.apk
-ls -la $TRAVIS_BUILD_DIR/platforms/android/app/build/outputs/apk/release
+ls -la $TRAVIS_BUILD_DIR/platforms/android/app/build/outputs/apk/debug
 
 cd ..
 # cd $TRAVIS_BUILD_DIR
