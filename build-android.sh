7098f378dc9988eb8ea174e4cd36638f8f9546ab
Nguoi An Phu <mrfly911@gmail.com>
2019-03-30 04:58:17 +0000

    BingBongTest release 2.0.1

--- build-android.sh
+++ build-android.sh
@@ -13,11 +13,22 @@ cordova plugin add cordova-plugin-whitelist
 cordova plugin ls
 
 echo "Building apk for Android"
-cordova build android
+cordova build android --release
 
 echo "Built the following apk(s):"
-# app-release-unsigned.apk
-ls -la $TRAVIS_BUILD_DIR/platforms/android/app/build/outputs/apk/debug
+cp -R $TRAVIS_BUILD_DIR/platforms/android/app/build/outputs/apk/release/* $TRAVIS_BUILD_DIR/
+cd $TRAVIS_BUILD_DIR
+ls -la
 
-cd ..
-# cd $TRAVIS_BUILD_DIR
+echo "Signing an App"
+
+echo "Generate the keytool"
+keytool -genkey -v -keystore android.keystore -alias android-app-key -keyalg RSA -keysize 2048 -validity 10000 -storepass ${MY_ZIP_PASSWORD} -noprompt
+
+echo "Sign the APK with the key we just created"
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore android.keystore app-release-unsigned.apk android-app-key -storepass ${MY_ZIP_PASSWORD} 
+
+echo "Optimize the APK file with the zipalign tool and also rename it to reflect the signing."
+sudo apt install zipalign -y
+zipalign -v 4 app-release-unsigned.apk app-release.apk
+ls -la
