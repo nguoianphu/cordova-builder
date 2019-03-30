e5c2f6b2aefd8c060ff110c0fb0e182130667d1f
Nguoi An Phu <mrfly911@gmail.com>
2019-03-30 06:16:33 +0000

    Upgrade Anroid SDK to 28.0.3

--- build-android.sh
+++ build-android.sh
@@ -18,6 +18,8 @@ cordova build android
 echo "Built the following apk(s):"
 # app-release-unsigned.apk
 ls -la $TRAVIS_BUILD_DIR/platforms/android/app/build/outputs/apk/debug
+cp -R $TRAVIS_BUILD_DIR/platforms/android/app/build/outputs/apk/debug/* .
+ls -la
 
 cd ..
 # cd $TRAVIS_BUILD_DIR
