c9eaa552e6c23be6da2e27e946dce41960540f33
nguoianphu <nguoianphu@gmail.com>
2019-07-30 09:14:47 +0000

    remove unsued

--- build-osx.sh
+++ build-osx.sh
@@ -1,12 +1,5 @@
 #!/bin/bash -v
 
-echo "Exit immediately if a command exits with a non-zero status."
-set -e
-echo "Don't print all commands"
-set +x
-
-echo "Travis CI set up"
-
 npm install
 npm install -g codecov
 cordova platform add osx
